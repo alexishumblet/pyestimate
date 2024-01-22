2a12eab11c7f8818771fb4b64905c770b25dfecc
alexishumblet <156506719+alexishumblet@users.noreply.github.com>
2024-01-22 21:38:58 +0100

    Updated docs

--- pyestimate/__init__.py
+++ pyestimate/__init__.py
@@ -1,6 +1,11 @@
 """
-pyestimate
 
+.. autosummary::
+    pyestimate.sin_param_estimate
+    pyestimate.sin2d_param_estimate
+    pyestimate.multiple_sin_param_estimate
+    pyestimate.pc_ar_estimator
+    
 """
 
 __version__ = "0.3.0"
